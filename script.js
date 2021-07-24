31d357976c5fef244d080f465b63d1ed4c721e81
Glitch (glitch-hello-website) <none>
2021-07-24 02:17:11 +0000

    🙏🎌 Checkpoint ./.glitch-assets:32136914/43 ./README.md:32136914/3353 ./index.html:32136914/4138 ./script.js:32136914/8567 ./p5.min.js:32136914/621362 ./p5.js:32136914/4332284 ./p5.collide2d.js:32136914/13008

--- script.js
+++ script.js
@@ -1,27 +1,96 @@
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
+// Name any p5.js functions we use in `global` so Glitch can recognize them.
+/* global
+ *    HSB, background, color, collideRectRect, colorMode, createCanvas, fill, frameRate, keyCode, height,
+ *    loop, noFill, noLoop, noStroke, random, rect, round, stroke, sqrt, text, width
+ *    UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW
+ */
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
+let backgroundColor, snake, apple, score;
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
-if (btn) {
-  btn.onclick = function() {
-    // The JS works in conjunction with the 'dipped' code in style.css
-    btn.classList.toggle("dipped");
-  };
+function setup() {
+  createCanvas(400, 400);
+  colorMode(HSB, 360, 100, 100);
+  frameRate(12);
+
+  backgroundColor = 95;
+  snake = new Snake();
+  apple = new Apple();
+  score = 0;
 }
 
-// This is a single line JS comment
-/*
-This is a comment that can span multiple lines 
-- use comments to make your own notes!
-*/
+function draw() {
+  background(backgroundColor);
+
+  snake.moveSelf();
+  snake.showSelf();
+  snake.checkCollisions();
+  snake.checkApples();
+
+  apple.showSelf();
+
+  displayScore();
+}
+
+function displayScore() {}
+
+class Snake {
+  constructor() {
+    this.size = 10;
+    this.x = width / 2;
+    this.y = height - 10;
+    this.direction = "N";
+    this.speed = 12;
+  }
+
+  moveSelf() {
+    if (this.direction === "N") {
+      this.y -= this.speed;
+    } else if (this.direction === "S") {
+      this.y += this.speed;
+    } else if (this.direction === "E") {
+      this.x += this.speed;
+    } else if (this.direction === "W") {
+      this.x -= this.speed;
+    } else {
+      console.log("Error: invalid direction");
+    }
+  }
+
+  showSelf() {
+    stroke(240, 100, 100);
+    noFill();
+    rect(this.x, this.y, this.size, this.size);
+    noStroke();
+  }
+
+  checkApples() {}
+
+  checkCollisions() {}
+
+  extendTail() {}
+}
+
+class Apple {
+  constructor() {}
+
+  showSelf() {}
+}
+
+function keyPressed() {
+  console.log("key pressed: ", keyCode);
+  if (keyCode === UP_ARROW && snake.direction != "S") {
+    snake.direction = "N";
+  } else if (keyCode === DOWN_ARROW && snake.direction != "N") {
+    snake.direction = "S";
+  } else if (keyCode === RIGHT_ARROW && snake.direction != "W") {
+    snake.direction = "E";
+  } else if (keyCode === LEFT_ARROW && snake.direction != "E") {
+    snake.direction = "W";
+  } else {
+    console.log("Error: invalid key");
+  }
+}
+
+function restartGame() {}
+
+function gameOver() {}
