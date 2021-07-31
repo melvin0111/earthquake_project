a955d24bd2fa27f1d39e363b8de408b18733b8b3
Glitch (smiling-deserted-penalty) <none>
2021-07-31 21:34:20 +0000

    🚤📕 Checkpoint ./script.js:33130574/3124

--- script.js
+++ script.js
@@ -4,6 +4,12 @@
  *    keyCode, height, loop, noLoop, noStroke, random, rect, round, stroke, text, width
  *    UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW, Mappa, loadJSON, clear, ellipse, createVector, createSlider
  */
+// Name any p5.js functions we use in `global` so Glitch can recognize them.
+/* global
+ *    HSB, background, collideRectRect, color, colorMode, createCanvas, fill, frameRate,
+ *    keyCode, height, loop, noLoop, noStroke, random, rect, round, stroke, text, width
+ *    UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW, Mappa, loadJSON, clear, ellipse, createVector, createSlider
+ */
 
 let canvas;
 let myMap;
@@ -25,22 +31,17 @@ let speed;
 let color;
 
 const options = {
-  lat: 40.73447,
-  lng: -74.00232,
-  zoom: 13,
+  lat: 40,
+  lng: 5,
+  zoom: 2,
   style: 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png'
 }
 const mappa = new Mappa('Leaflet');
 
-function preload() {
-  data = loadJSON('./data/taxiday1.geojson');
-}
-
 function setup() {
-  canvas = createCanvas(600, 600);
+  canvas = createCanvas(1000, 600);
   myMap = mappa.tileMap(options);
   myMap.overlay(canvas); 
-  tripsCoordinates = myMap.geoJSON(data, "LineString");
 
   tripsCoordinates.forEach(function (trip) {
     trip.forEach(function (coordinate) {
@@ -48,48 +49,9 @@ function setup() {
       })
   });
   
-  size = createSlider(5, 20);
-  size.position(10, 610);
-  size.style('width', '300px');
-  
-  speed = createSlider(0.2, 1, 0.2, 0.01);
-  speed.position(10, 650);
-  speed.style('width', '300px');
-  
-  color = createSlider(0, 250);
-  color.position(10, 690);
-  color.style('width', '300px');
-  
-  //myMap.onChange(drawPoints);
 }
 
 
 function draw(){
-  clear();
-  if(delta < 1){
-    delta += speed.value(); 
-  } else {
-    delta = 0; 
-    coordinate ++; 
-  }
 
-  origin = myMap.latLngToPixel(allCoordinates[coordinate][1], allCoordinates[coordinate][0]); 
-  originVector = createVector(origin.x, origin.y); 
-  destination = myMap.latLngToPixel(allCoordinates[coordinate + 1][1], allCoordinates[coordinate + 1][0]);  
-  destinationVector = createVector(destination.x, destination.y);
-
-  taxiPosition = originVector.lerp(destinationVector, delta);
-  colorMode(HSB)
-  fill(color.value(),100,100);
-  ellipse(taxiPosition.x, taxiPosition.y, size.value(), size.value());
-}
-
-function drawPoints(){
-  clear() 
-  noStroke();
-  fill(255);
-  for(let i = 0; i < allCoordinates.length; i++){
-    let pos = myMap.latLngToPixel(allCoordinates[i][1], allCoordinates[i][0])
-    ellipse(pos.x, pos.y, 15, 15);
-  }
 }
