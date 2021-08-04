a71fa8366ef0281321092ae8a52efb68adbcaf1f
Glitch (smiling-deserted-penalty) <none>
2021-08-04 23:27:24 +0000

    📻😚 Checkpoint ./script.js:32694160/5070:33130574/670

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // Name any p5.js functions we use in `global` so Glitch can recognize them.
 /* global
  *    HSB, background, collideRectRect, color, colorMode, createCanvas, fill, frameRate, earthquakes,
@@ -12,50 +11,64 @@ let dateSlider;
 let dateText;
 let canvasWidth;
 let canvasHeight;
-  
+
 const options = {
   lat: 40,
   lng: 5,
   zoom: 2,
-  style: 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png'
-}
-const mappa = new Mappa('Leaflet');
-
+  style: "http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png"
+};
+const mappa = new Mappa("Leaflet");
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   canvasWidth = 800;
   canvasHeight = 600;
-  
-  
+
   // canvas = createCanvas(canvasWidth, canvasHeight);
   myMap = mappa.tileMap(options);
-  myMap.overlay(canvas); 
+  myMap.overlay(canvas, function() {
+    // Logan: Trying out a resizing solution from https://github.com/cvalenzuela/Mappa/issues/31
+    myMap.map.invalidateSize();
+  });
   dateSlider = createSlider(1965, 2016, 1965, 1);
-  
-  dateSlider.style('width', '300px');
+  dateSlider.position(10, windowHeight - 50);
+  dateSlider.style("width", "300px");
 }
 
-function draw(){
-  clear(); 
-  /* this needs to be used to reset the position of the circles every 
-  *  frame, so it is updated when the tile map is moved/resized
-  */
+function draw() {
+  clear();
+  /* this needs to be used to reset the position of the circles every
+   *  frame, so it is updated when the tile map is moved/resized
+   */
   colorMode(HSB);
   fill(0, 0, 100);
   text(`Year: ${dateSlider.value()}`, 10, windowHeight - 60);
   for (let i = 0; i < earthquakes.length; i++) {
     if (Number(earthquakes[i].Date.slice(-2)) === dateSlider.value() % 100) {
-      let pixels = myMap.latLngToPixel(earthquakes[i].Latitude, earthquakes[i].Longitude);
+      // Options to support dynamic window resizing:
+      //   A. Store initial width/height, and adjust pixel values based on the current and initial sizes
+      //   B. Figure out how to properly tell mappa to resize the map
+      let pixels = myMap.latLngToPixel(
+        earthquakes[i].Latitude,
+        earthquakes[i].Longitude
+      );
       colorMode(HSB);
-      fill(100, 100, 100);
-      ellipse(pixels.x, pixels.y, (10 ** (earthquakes[i].Magnitude / 3.2)) / (10));
+      let hue = 360 - (earthquakes[i].Magnitude * 36) % 360;
+      fill(hue, 100, 100);
+      ellipse(pixels.x, pixels.y, 10 ** (earthquakes[i].Magnitude / 3.2) / 10);
+      if (i == 0) {
+        console.log(hue);
+      }
     }
   }
 }
 
 function windowResized() {
+  text(`Year: ${dateSlider.value()}`, 10, windowHeight - 60);
   resizeCanvas(windowWidth, windowHeight);
   dateSlider.position(10, windowHeight - 50);
+  
+  myMap.mappaDiv.style.width = canvas.width + 'px';
+      myMap.mappaDiv.style.height = canvas.height + 'px';
 }
-
